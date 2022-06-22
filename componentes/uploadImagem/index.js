c8477823c745185f9788c49a8cb9377f411cbfae
Zurceel <gabrielcruzsil@gmail.com>
2022-06-22 14:19:47 -0300

    Iniciando o projeto devagram, inserindo upload de imagens.

---
+++ componentes/uploadImagem/index.js
@@ -0,0 +1,60 @@
+import { useEffect, useRef } from "react";
+
+export function UploadImagem({
+    className = '',
+    setImagem,
+    imagemPreview,
+    imagemPreviewClassName = '',
+    aoSetarAreferencia
+}) {
+    const referenciaInput = useRef(null);
+
+    useEffect(() => {
+        if (!aoSetarAreferencia) {
+            return;
+        }
+
+        aoSetarAreferencia(referenciaInput?.current)
+    }, [referenciaInput?.current]);
+
+    const abrirSeletordeArquivos = () => {
+        referenciaInput?.current?.click();
+    }
+
+    const aoAlterarImagem = () => {
+        if (!referenciaInput?.current?.files?.length) {
+            return;
+        }
+
+        const arquivo = referenciaInput?.current?.files[0];
+        const fileReader = new FileReader();
+        fileReader.readAsDataURL(arquivo);
+        fileReader.onloadend = () => {
+            setImagem({
+                preview: fileReader.result,
+                arquivo
+            });
+        }
+    }
+
+    return (
+        <div className={`uploadImagemContainer`} onClick={abrirSeletordeArquivos}>
+            {imagemPreview && (
+                <div className="imagemPreviewContainer">
+                    <img
+                        src={imagemPreview}
+                        alt='Imagem Preview'
+                        className={imagemPreviewClassName}
+                    />
+                </div>
+            )}
+            <input
+                type='file'
+                className='oculto'
+                accept="image/*"
+                ref={referenciaInput}
+                onChange={aoAlterarImagem}
+            />
+        </div>
+    );
+}
